65c9fa6539d6454a3518df7d7f54abd852bdb100
frankieandone <22060147+frankieandone@users.noreply.github.com>
2024-06-16 13:41:05 +0100

    Chore: update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -57,5 +57,11 @@ return {
                 "kotlin",
             },
         },
+        "phaazon/hop.nvim",
+        as = "hop",
+        config = function()
+            -- you can configure Hop the way you like here; see :h hop-config
+            require("hop").setup { keys = "etovxqpdygfblzhckisuran" }
+        end,
     },
 }
